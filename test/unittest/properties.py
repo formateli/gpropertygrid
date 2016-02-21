b1192fdbf3dbfbe497e17d682c94bcc2ea9d1aa8
formateli <formateli@gmail.com>
2016-02-20 20:33:36 -0500

    new: unittest setup !wip

---
+++ test/unittest/properties.py
@@ -0,0 +1,9 @@
+# This file is part of GPropertyGrid project.
+# The COPYRIGHT file at the top level of this repository
+# contains the full copyright notices and license terms.
+
+import unittest
+
+class PropertiesTest(unittest.TestCase):
+    def testProperties(self):
+        pass
